12ce7b4d07724dee005c4ff910abc0a87cfd35ee
John Snow <johnsnow@richards-imac.lan>
2018-12-17 08:24:08 -0600

    Finished Module 4 Lesson 4

---
+++ src/main/java/com/handy/aws/domain/Product.java
@@ -0,0 +1,56 @@
+package com.handy.aws.domain;
+
+public class Product {
+	int product_id;
+	String toolType;
+	String brand;
+	String name;
+	int count;
+	public Product() {
+		
+	}
+	public Product(int product_id, String toolType, String brand, String name, int count) {
+		this.product_id = product_id;
+		this.toolType = toolType;
+		this.brand = brand;
+		this.name = name;
+		this.count = count;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getToolType() {
+		return toolType;
+	}
+	public void setToolType(String toolType) {
+		this.toolType = toolType;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	@Override
+	public String toString() {
+		return "Product [product_id=" + product_id + ", toolType=" + toolType + ", brand=" + brand + ", name=" + name
+				+ ", count=" + count + "]";
+	}
+	
+
+}
